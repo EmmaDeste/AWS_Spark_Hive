da98c389199e6a99fa5a48e6df3980ee356bfe66
EmmaDeste <emma_deste@hotmail.com>
2025-01-27 19:58:15 +0100

    add Data Analysis part

--- hive-etl.hql
+++ hive-etl.hql
@@ -12,3 +12,42 @@ ROW FORMAT DELIMITED
 FIELDS TERMINATED BY ','
 STORED AS TEXTFILE
 LOCATION 's3://dai-2025-paris/deste/output/movielens/moviesInfoDf.csv/';
+
+/* ########################### DATA ANALYSIS ####################################### */
+
+WITH best_movies_per_year AS (
+    SELECT *, ROW_NUMBER() OVER (PARTITION BY m.releaseyear ORDER BY m.rating_average DESC) AS rank
+    FROM moviesInfo m
+)
+SELECT *
+FROM best_movies_per_year
+WHERE rank = 1
+ORDER BY releaseyear;
+
+WITH best_movies_per_genre AS (
+    SELECT *, ROW_NUMBER() OVER (PARTITION BY m.genre ORDER BY m.rating_average DESC) AS rank
+    FROM moviesInfo m
+)
+SELECT *
+FROM best_movies_per_genre
+WHERE rank = 1
+ORDER BY genre;
+
+WITH best_action_movies_per_year AS (
+    SELECT *, ROW_NUMBER() OVER (PARTITION BY m.releaseyear ORDER BY m.rating_average DESC) AS rank
+    FROM moviesInfo m
+    WHERE m.genre LIKE '%Action%')
+SELECT *
+FROM best_action_movies_per_year
+WHERE rank = 1
+ORDER BY releaseyear;
+
+WITH best_romance_movies_per_year AS (
+    SELECT*, ROW_NUMBER() OVER (PARTITION BY m.releaseyear ORDER BY m.rating_average DESC) AS rank
+    FROM moviesInfo m
+    WHERE m.genre LIKE '%Romance%'
+)
+SELECT *
+FROM best_romance_movies_per_year
+WHERE rank = 1
+ORDER BY releaseyear;
--- spark-etl.py
+++ spark-etl.py
@@ -42,3 +42,30 @@ moviesInfoDf = moviesInfoDf.filter(moviesInfoDf["Genre"] != '(no genres listed)'
 # 3 - LOAD
 
 moviesInfoDf.write.mode("overwrite").option("header", "true").csv("s3://dai-2025-paris/deste/output/movielens/moviesInfoDf.csv")
+
+# ########################### DATA ANALYSIS #######################################
+
+clean_movies_path = "s3://dai-2025-paris/deste/output/movielens/moviesInfoDf.csv/"
+movies_df = spark.read.csv(clean_movies_path, header=True, inferSchema=True)
+
+from pyspark.sql.functions import col, row_number
+from pyspark.sql.window import Window
+releaseYear_window_descRating = Window.partitionBy("releaseYear").orderBy(col("averageRating").desc())
+ranked_movies = movies_df.withColumn("rank", row_number().over(releaseYear_window_descRating))
+best_movies_per_year = ranked_movies.filter(col("rank") == 1)
+best_movies_per_year.show()
+
+genre_window_descRating = Window.partitionBy("Genre").orderBy(col("averageRating").desc())
+ranked_movies = movies_df.withColumn("rank", row_number().over(genre_window_descRating))
+best_movies_per_genre = ranked_movies.filter(col("rank") == 1)
+best_movies_per_genre.show()
+
+action_movies_df = movies_df.filter(movies_df.Genre == "Action")
+ranked_action_movies = action_movies_df.withColumn("rank", row_number().over(releaseYear_window_descRating))
+best_action_movies_per_year = ranked_action_movies.filter(col("rank") == 1)
+best_action_movies_per_year.show()
+
+romance_movies_df = movies_df.filter(movies_df.Genre == "Romance")
+ranked_romance_movies = romance_movies_df.withColumn("rank", row_number().over(releaseYear_window_descRating))
+best_romance_movies_per_year = ranked_romance_movies.filter(col("rank") == 1)
+best_romance_movies_per_year.show()
